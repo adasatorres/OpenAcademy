627f0e40877d9174da6136ae3f45de28a47a7f8f
adasatorres <128601880+adasatorres@users.noreply.github.com>
2023-04-11 13:16:53 +0100

    V4

--- crm_open_academy/models/lead.py
+++ crm_open_academy/models/lead.py
@@ -1,18 +1,21 @@
 from odoo import models, fields, api
+from odoo.http import request
 import logging
 _logger = logging.getLogger(__name__)
 class Lead(models.Model):
     _inherit = 'crm.lead'
+    curso_id = fields.Many2one(comodel_name='openacademy.cursos', string="Curso")
 
     def send_email(self):
         ids = self.env.ref('crm_open_academy.confirmacion_template_email').id
         template = self.env['mail.template'].browse(ids)
         template.send_mail(self.id,force_send=True)
     
-    
+
     @api.model
-    def create(self, args):
+    def create(self, args):      
         lead = super(Lead, self).create(args)
-        if int(args['medium_id']) == 1:
+        _logger.info('####### {0}'.format(args))
+        if 'active' in args.keys():
             lead.send_email()
         return lead
